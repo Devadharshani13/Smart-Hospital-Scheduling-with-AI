b33f0a321e6bbd9cf45db9848fbbe37447fbff22
Devadharshani B <devadharshai13@gmail.com>
2026-01-11 00:18:56 +0530

    Update utils.js

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,7 +1,6 @@
-import { cn } from '../../lib/utils';
-import { twMerge } from "tailwind-merge"
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
-
